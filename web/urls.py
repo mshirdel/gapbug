5c157ff259d83c132b38309cd7f899be16377956
mshirdel <mshirdel@gmail.com>
2021-03-16 19:19:03 +0330

    refactor home view

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,10 @@
 from django.views.generic import TemplateView
 from django.urls import path
+from . import views
 
 
 app_name = 'web'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="web/index.html"),
-         name="home"),
+    path('', views.index, name="home"),
 ]
