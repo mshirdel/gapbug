b2384b86e67b39a2be7e037074a09918ec53cfb4
mshirdel <mshirdel@gmail.com>
2021-04-12 21:14:17 +0430

    fix some minor bugs

--- web/views.py
+++ web/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'qa/index.html')
+        # return render(request, 'qa/index.html')
+        return HttpResponseRedirect(reverse('qa:index'))
     else:
         return render(request, 'web/index.html')
