8b4eb22dcd8557ceb8d89841f5f6022759f0b978
mshirdel <mshirdel@gmail.com>
2021-04-17 23:56:01 +0430

    fix bug in sending email for reset password

--- gapbug/settings.py
+++ gapbug/settings.py
@@ -133,6 +133,7 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = 'info@porseshdev.ir'
 
 try:
     from .settings_dev import *
