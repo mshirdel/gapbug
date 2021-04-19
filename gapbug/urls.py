94fdbfee2675b126a82a64505f5c1ba42572a105
mshirdel <mshirdel@gmail.com>
2021-04-19 19:11:25 +0430

    fix some minor error and ui change

--- gapbug/urls.py
+++ gapbug/urls.py
@@ -4,12 +4,7 @@ from django.conf import settings
 import debug_toolbar
 
 
-if settings.DEBUG:
-    urlpatterns = [
-        path('rosetta/', include('rosetta.urls')),
-    ]
-
-urlpatterns += [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('account.urls')),
     path('', include('web.urls', namespace='web')),
@@ -17,3 +12,8 @@ urlpatterns += [
     path('profile/', include('user_profile.urls', namespace='user_profile')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('rosetta/', include('rosetta.urls')),
+    ]
