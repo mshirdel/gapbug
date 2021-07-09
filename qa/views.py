378bf602a5665993b410622c1f13a0e285ec73c2
mshirdel <mshirdel@gmail.com>
2021-07-10 00:53:20 +0430

    update readme file. Show number of questions in home question page

--- qa/views.py
+++ qa/views.py
@@ -28,6 +28,12 @@ class QuestionList(ListView):
     paginate_by = settings.PAGE_SIZE
     template_name = 'qa/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['heading_title'] = \
+            f"{_('Questions')} ({Question.objects.count()})"
+        return context
+
 
 @method_decorator(login_required, name='dispatch')
 class Ask(PrivilageRequiredMixin, View):
