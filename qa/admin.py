e14f689ef3c1b090fb06ed4e0fb8d0ab44dcfeee
MojixCoder <mojix1010@gmail.com>
2021-07-16 14:47:02 +0430

    Add Comment Model

--- qa/admin.py
+++ qa/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Comment
 
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("title", "user")
-
-
+    
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Comment)
--- qa/models.py
+++ qa/models.py
@@ -1,11 +1,14 @@
 from django.utils import timezone
 from django.db import models
-from django.db.models import Sum
+from django.db.models import Sum, constraints
 from django.utils.text import slugify
 from django.urls import reverse
 from common.models import TimeStampModel
 from django.conf import settings
 from taggit.managers import TaggableManager
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class Question(TimeStampModel):
@@ -106,9 +109,56 @@ class AnswerVote(Vote):
 
     class Meta:
         unique_together = ["user", "answer"]
+        #TODO use models.UniqueConstraint instead of unique_together.
+        # check out this link for the reason https://docs.djangoproject.com/en/dev/ref/models/options/#unique-together
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         qs = AnswerVote.objects.filter(answer=self.answer)
         self.answer.vote = qs.aggregate(Sum("rate"))["rate__sum"]
         self.answer.save()
+
+
+class Comment(TimeStampModel):
+    """ 
+    Comment Model
+        Note:
+            Supports leaving comments for Questions or Answers.
+    """
+    # Foreign Keys
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
+    # Comment Fields
+    text = models.TextField(
+        validators=[
+            MinLengthValidator(15),
+            MaxLengthValidator(600),
+        ]
+    )
+    vote = models.SmallIntegerField()
+    
+    def __str__(self):
+        return f"{self.user.username}-{self.text[:14]}"
+    
+    class Meta:
+        ordering = "-created",
+        
+
+class CommentVote(Vote):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="comment_votes")
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'comment'], name='unique_comment_vote')
+        ]
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        qs = CommentVote.objects.filter(comment=self.comment)
+        self.comment.vote = qs.aggregate(Sum("rate"))["rate__sum"]
+        self.comment.save()
+        
