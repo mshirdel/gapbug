41788296ce179bf537634d8f01d10b23cebea6d3
MojixCoder <mojix1010@gmail.com>
2021-07-17 19:49:25 +0430

    replaced production.py with the default one

--- gapbug/settings/production.py
+++ gapbug/settings/production.py
@@ -1,31 +1,11 @@
 from .base import *
 
-SECRET_KEY = 'asdasdasdasds'
 
-ALLOWED_HOSTS = ["*"]
-
-DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gapbug', 
-        'USER': os.getenv("db_user", "postgres"), 
-        'PASSWORD': os.getenv("db_password", "pawndarby"),
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-        'TEST': {
-            'NAME': 'gapbug_test',
-        },
-    }
-}
-
-INSTALLED_APPS.append('rosetta')
-INSTALLED_APPS.append('django_extensions')
-INSTALLED_APPS.append('debug_toolbar')
-
-MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_INFO = 'email_name@example.com'
-DEFAULT_FROM_EMAIL = 'email_name@example.com'
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+EMAIL_INFO = "info@porseshdev.ir"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+DEFAULT_FROM_EMAIL = "info@porseshdev.ir"
